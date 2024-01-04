166493d4fdd3c9241b426626c1bf2b0fc87d7c63
tuanngocfun <tuanngoccs50@gmail.com>
2024-01-04 12:17:41 +0700

    refactoring code, with if conditional inversion and de morgan rule also creating new helper method"

---
+++ csharpcore/GildedRose/GildedRose.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GildedRoseKata
+{
+    class ItemUpdater
+    {
+        public virtual void UpdateQuality(Item item) 
+        {
+            // Base implementation or make abstract if no base implementation
+        }
+
+        public virtual void UpdateSellIn(Item item) 
+        {
+            // Base implementation or make abstract if no base implementation
+        }
+    }
+
+    class DefaultItemUpdater : ItemUpdater
+    {
+        private readonly Action<Item, int> _decreaseQualityAction;
+
+        public DefaultItemUpdater(Action<Item, int> decreaseQualityAction)
+        {
+            _decreaseQualityAction = decreaseQualityAction;
+        }
+
+        public override void UpdateQuality(Item item) 
+        {
+            _decreaseQualityAction(item, 1);
+            if (item.SellIn < 0)
+            {
+                _decreaseQualityAction(item, 1);
+            }
+        }
+        public override void UpdateSellIn(Item item) 
+        {
+            item.SellIn--;
+        }
+    }
+
+    class AgedBrieUpdater : DefaultItemUpdater 
+    {
+        public AgedBrieUpdater(Action<Item, int> decreaseQualityAction) : base(decreaseQualityAction) { }
+
+        public override void UpdateQuality(Item item) 
+        {
+            GildedRose.IncreaseItemQuality(item);
+            if (item.SellIn < 0)
+            {
+                GildedRose.IncreaseItemQuality(item);
+            }
+        }
+    }
+
+    class BackstagePassesUpdater : DefaultItemUpdater 
+    {
+        public BackstagePassesUpdater(Action<Item, int> decreaseQualityAction) : base(decreaseQualityAction) { }
+
+        public override void UpdateQuality(Item item) 
+        {
+            GildedRose.IncreaseItemQuality(item);
+            if (item.SellIn < 10)
+            {
+                GildedRose.IncreaseItemQuality(item);
+            }
+            if (item.SellIn < 5)
+            {
+                GildedRose.IncreaseItemQuality(item);
+            }
+            if (item.SellIn < 0)
+            {
+                item.Quality = 0;
+            }
+        }
+    }
+
+    class SulfurasUpdater : ItemUpdater 
+    {
+        public override void UpdateQuality(Item item) { }
+        public override void UpdateSellIn(Item item) { }
+    }
+
+
+    public class GildedRose
+    {
+        public const string AGED_BRIE = "Aged Brie";
+        public const string BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+        public const string SULFURAS = "Sulfuras, Hand of Ragnaros";
+
+        private readonly IList<Item> _items;
+        private readonly Dictionary<string, ItemUpdater> _itemUpdaters;
+
+        public GildedRose(IList<Item> items)
+        {
+            _items = items;
+            _itemUpdaters = new Dictionary<string, ItemUpdater>
+            {
+                { AGED_BRIE, new AgedBrieUpdater(DecreaseItemQuality) },
+                { BACKSTAGE_PASSES, new BackstagePassesUpdater(DecreaseItemQuality) },
+                { SULFURAS, new SulfurasUpdater() },
+                // Default updater for items that don't match any special categories
+                { "default", new DefaultItemUpdater(DecreaseItemQuality) }
+            };
+        }
+
+        internal static void DecreaseItemQuality(Item item, int amount = 1)
+        {
+            item.Quality = Math.Max(0, item.Quality - amount);
+        }
+
+        internal static void IncreaseItemQuality(Item item, int amount = 1, int max_quality = 50)
+        {
+            item.Quality = Math.Min(max_quality, item.Quality + amount);
+        }
+
+        private static void UpdateQualitySingle(Item item)
+        {
+            if (item.Name == SULFURAS)
+            {
+                return;
+            } else {
+                item.SellIn--;
+            }
+
+            if (item.Name == AGED_BRIE)
+            {
+                IncreaseItemQuality(item);
+                if (item.SellIn < 0)
+                {
+                    IncreaseItemQuality(item);
+                }
+            } 
+            else if (item.Name == BACKSTAGE_PASSES)
+            {
+                IncreaseItemQuality(item);
+                if (item.SellIn < 10)
+                {
+                    IncreaseItemQuality(item);
+                }
+                if (item.SellIn < 5)
+                {
+                    IncreaseItemQuality(item);
+                }
+                if (item.SellIn < 0)
+                {
+                    item.Quality = 0;
+                }
+            } else if (item.Name == SULFURAS) {
+                return;
+            } else {
+                DecreaseItemQuality(item);
+                if (item.SellIn < 0)
+                {   
+                    DecreaseItemQuality(item);
+                }
+            }
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (var item in _items)
+            {
+                UpdateQualitySingle(item);
+            }
+        }
+    }
+}
